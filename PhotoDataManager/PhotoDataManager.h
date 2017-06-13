aeaf8e293eafdfdd0480ebcf868683b3cdf7208c
mnaseem001 <mnaseem001@yahoo.com>
2017-06-13 17:24:52 -0400

    Initial Commit

---
+++ PhotoDataManager/PhotoDataManager.h
@@ -0,0 +1,19 @@
+//
+//  PhotoDataManager.h
+//  PhotoDataManager
+//
+//  Created by Mansoor Naseem on 6/13/17.
+//  Copyright © 2017 Mansoor Naseem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PhotoDataManager.
+FOUNDATION_EXPORT double PhotoDataManagerVersionNumber;
+
+//! Project version string for PhotoDataManager.
+FOUNDATION_EXPORT const unsigned char PhotoDataManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoDataManager/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,18 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '9.0'
+use_frameworks!
+
+target 'PhotoViewer' do
+    pod 'AlamofireObjectMapper', '~> 4.0'
+    pod 'Kingfisher', '~> 3.0'
+end
+
+target 'PhotoDataManager' do
+    pod 'AlamofireObjectMapper', '~> 4.0'
+    pod 'Kingfisher', '~> 3.0'
+end
+
+target 'PhotoDataManagerTests' do
+    pod 'AlamofireObjectMapper', '~> 4.0'
+    pod 'Kingfisher', '~> 3.0'
+end
