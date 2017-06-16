379140d281661ebcba08e8a823af80d8a3402dcf
mnaseem001 <mnaseem001@yahoo.com>
2017-06-15 23:06:09 -0400

    AppDelegate: loadData from app did become active instead of didFinishLaunchingWithOptions

--- PhotoViewer/AppDelegate.swift
+++ PhotoViewer/AppDelegate.swift
@@ -16,23 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        // Use the PhotoDataManager Package
-        // Get sharedInstance (a singleton)
-        // - use the server url dependency for this app
-        // After fetch is done successful or failed send notification
-        // Prefect all thumbnail images
-        // - Takes rougly over 67 seconds to download 5000 thumbnail images on a very good WIFI
-        // - NOTE: Prefetch may need be turned off in case network issue
-        let manager = PhotoDataManager.sharedInstanceWith(urlString: PhotoViewerConstants.kPhotoServerUrlString)
-        manager.fetchPhotoData { (photoDataArray, error) in
-            NotificationCenter.default.post(name: Notification.Name(PhotoViewerConstants.kNotificationFetchedPhotosDone), object: nil)
-            
-            // Prefetch All images
-            manager.prefetchPhotoImages(completion: { (completedResource) in
-                // Custom code if necessary
-            })
-        }
+        print("didFinishLaunchingWithOptions")
+
         
         return true
     }
@@ -49,16 +34,65 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("applicationWillEnterForeground")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("applicationDidBecomeActive")
+        // Use the PhotoDataManager Package
+        // Get sharedInstance (a singleton)
+        // - use the server url dependency for this app
+        // After fetch is done successful or failed send notification
+        // Prefect all thumbnail images
+        // - Takes rougly over 67 seconds to download 5000 thumbnail images on a very good WIFI
+        // - NOTE: Prefetch may need be turned off in case network issue
+        let manager = PhotoDataManager.sharedInstanceWith(urlString: PhotoViewerConstants.kPhotoServerUrlString)
+        manager.fetchPhotoData { (photoDataArray, error) in
+            
+            if error == nil {
+                NotificationCenter.default.post(name: Notification.Name(PhotoViewerConstants.kNotificationFetchedPhotosDone), object: nil)
+                // Prefetch All images
+                manager.loadImages()
+                
+            } else {
+                
+                self.showFetchDataError(error: error)
+                NotificationCenter.default.post(name: Notification.Name(PhotoViewerConstants.kNotificationFetchedPhotosFailed), object: nil)
+                
+            }
+            
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    
+    fileprivate func showFetchDataError (error: Error?)  {
+        
+        let title = NSLocalizedString("Fetching Data Error", comment: "")
+        var errorString = "No Error Returned."
+        
+        if let error = error {
+            errorString = error.localizedDescription
+        }
+        
+        let message = NSLocalizedString("Something went wrong fetching the data. Maybe you are not connected. Exact Error from the call: \(errorString)", comment: "")
+        
+        let action = UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: {(alert: UIAlertAction!) in
+            // Custom code after tapping OK
+        })
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(action)
+        
+        self.window?.rootViewController?.present(alertController, animated: true) {
+            
+        }
+        
+    }
 
 }
 
